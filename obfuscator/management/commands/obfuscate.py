d7049dffbc014a52e4dc3557b39faa641c44c0e0
Sandro Rodrigues <srtabs@gmail.com>
2017-08-07 18:46:23 +0100

    Added string and email obfuscation.

--- obfuscator/management/commands/obfuscate.py
+++ obfuscator/management/commands/obfuscate.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import hashlib
+
 from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
 from django.db import models
@@ -33,30 +35,56 @@ class Command(BaseCommand):
         """ """
         naturalkey = options.get('model')
         fields = options.get('fields', None)
-        model_class = self.get_model_class(naturalkey)
-        if self.validate_fields(model_class, fields):
-            data = {}
-            for obj in model_class._default.only(*fields).all():
+        model_class = self._get_model_class(naturalkey)
+        if self._validate_fields(model_class, fields):
+            for obj in model_class.objects.only(*fields).all():
+                data = {}
                 for field_name in fields:
                     field = model_class._meta.get_field(field_name)
+                    value = getattr(obj, field_name)
                     if isinstance(field, models.EmailField):
-                        data[field_name] = self.obfuscate_email()
+                        data[field_name] = self.obfuscate_email(
+                            value, field.max_length)
+                    elif isinstance(field, models.CharField):
+                        data[field_name] = self.obfuscate_text(
+                            value, field.max_length)
+                    elif isinstance(field, models.TextField):
+                        data[field_name] = self.obfuscate_text(value)
+                    elif isinstance(field, models.IntegerField):
+                        data[field_name] = self.obfuscate_int(value)
+                    else:
+                        raise TypeError("Invalid type '{}' for field '{}'"
+                                        .format(type(field), field_name))
+                model_class.objects.filter(pk=obj.pk).update(**data)
 
     def obfuscate_email(self, value, max_length):
         """ """
+        username, domain = value.split('@')
+        username = hashlib.sha224(username).hexdigest()
+        length = len(username) + len(domain) + 1
+        if length > max_length:
+            username = username[:(max_length - length)]
+        return "{username}@{domain}".format(
+            username=username, domain=domain)
 
-    def validate_fields(self, model_class, fields):
+    def obfuscate_text(self, value, max_length=None):
+        """ """
+        hashed_value = hashlib.sha224(value).hexdigest()
+        length = len(hashed_value)
+        if max_length and length > max_length:
+            hashed_value = hashed_value[:(max_length - length)]
+        return hashed_value
+
+    def obfuscate_int(self, value, unique=False):
+        """ """
+        pass
+
+    def _validate_fields(self, model_class, fields):
         # check if all fields are valid and exists for the model_class
         invalid_fields = []
         valid_fields = [field.name for field in model_class._meta.get_fields()]
         for field_name in fields:
-            if field_name in valid_fields:
-                field = model_class._meta.get_field(field_name)
-                if type(field) not in VALID_FIELDS_CLASS:
-                    raise TypeError(
-                        "Invalid type '{}' for field '{}'"
-                        .format(type(field), field_name))
-            else:
+            if field_name not in valid_fields:
                 invalid_fields.append(field_name)
         if invalid_fields:
             raise ValueError("Invalid fields for model '{}': {}"
@@ -64,11 +92,12 @@ class Command(BaseCommand):
                                      ",".join(invalid_fields)))
         return True
 
-    def get_model_class(self, option):
+    def _get_model_class(self, option):
         naturalkey = option.split('.')
         if len(naturalkey) <= 1:
             raise ValueError(
                 "Invalid model name. Valid formar: app_label.ModelClass")
         app_label = '.'.join(naturalkey[:-1]).lower()
         model = naturalkey[-1].lower()
-        return ContentType.objects.get_by_natural_key(app_label, model)
+        return ContentType.objects.get_by_natural_key(app_label, model)\
+            .model_class()
